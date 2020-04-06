95fee3f00fe57450ac0b8ce32813679457da3f9f
fadlanmp <fadlanmulya@gmail.com>
2020-04-06 22:56:17 +0700

    tugas6

---
+++ AnalgoKu6/soal3.cpp
@@ -0,0 +1,48 @@
+/*
+Nama Program		: Program BFS
+Nama			: Fadlan Mulya Priatna
+NPM			: 140810180041
+Tanggal Pembuatan	: 6 April 2020
+Deskripsi Program	: Progam ini berisi Breadth First Search  menggunakan bahasa C++
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int vertexSize = 8;
+	int adjacency[8][8] = {
+		{0,1,1,0,0,0,0,0},
+		{1,0,1,1,1,0,0,0},
+		{1,1,0,0,1,0,1,1},
+		{0,1,0,0,1,0,0,0},
+		{0,1,1,1,0,1,0,0},
+		{0,0,0,0,1,0,0,0},
+		{0,0,1,0,0,0,0,1},
+		{0,0,1,0,0,0,1,0}
+	};
+	bool discovered[vertexSize];
+	for(int i = 0; i < vertexSize; i++){
+		discovered[i] = false;
+	}
+	int output[vertexSize];
+
+	//inisialisasi start
+	discovered[0] = true;
+	output[0] = 1;
+
+	int counter = 1;
+	for(int i = 0; i < vertexSize; i++){
+		for(int j = 0; j < vertexSize; j++){
+			if((adjacency[i][j] == 1)&&(discovered[j] == false)){
+				output[counter] = j+1;
+				discovered[j] = true;
+				counter++;
+			}
+		}
+	}
+	cout<<"BFS : "<<endl;
+	for(int i = 0; i < vertexSize; i++){
+		cout<<output[i]<<" ";
+	}
+}
